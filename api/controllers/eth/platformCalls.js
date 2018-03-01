e686f3ea56948dfc700e4187269a99b4bc956df7
shessenauer <sam.hessenauer@gmail.com>
2018-03-01 14:33:59 -0800

    added skeleton code for view all tournaments eth calls

--- api/controllers/eth/platformCalls.js
+++ api/controllers/eth/platformCalls.js
@@ -13,6 +13,10 @@ const Web3 = require('web3')
 const config = require('../../../config');
 const externalApiCalls = require('./externalApiCalls')
 
+/*
+Uncomment this snippet when the contract ABI and address is already deployed on the private chain and matryx health monitor
+*/
+/*
 const matryxABI = externalApiCalls.platformInfoApiCall().then((result, error) => {
     if(!error){
         // console.log(result);
@@ -24,9 +28,12 @@ const matryxABI = externalApiCalls.platformInfoApiCall().then((result, error) =>
         console.log("Error during get Platform API call", error);
     }
 });
+*/
 
+/*
+Uncomment
 
-
+*/
 //Develop branch
 /*
 const matryxAbi = [{"constant":true,"inputs":[],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_author","type":"address"},{"name":"_submission","type":"address"}],"name":"updateMySubmissions","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tournamentAddress","type":"address"},{"name":"_finalRoundNumber","type":"uint256"},{"name":"_winningSubmissionIndex","type":"uint256"}],"name":"invokeTournamentClosedEvent","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"myTournaments","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"balanceIsNonZero","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_sender","type":"address"}],"name":"isOwner","outputs":[{"name":"_isOwner","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"tournamentCount","outputs":[{"name":"_tournamentCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_owner","type":"address"},{"name":"_tournamentAddress","type":"address"},{"name":"_tournamentName","type":"string"},{"name":"_externalAddress","type":"bytes32"},{"name":"_MTXReward","type":"uint256"},{"name":"_entryFee","type":"uint256"}],"name":"invokeTournamentOpenedEvent","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tournamentName","type":"string"},{"name":"_externalAddress","type":"bytes32"},{"name":"_MTXReward","type":"uint256"},{"name":"_entryFee","type":"uint256"}],"name":"createTournament","outputs":[{"name":"_tournamentAddress","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"_owner","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_queryID","type":"uint256"},{"name":"_response","type":"uint256"}],"name":"storeQueryResponse","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_index","type":"uint256"}],"name":"getTournamentAtIndex","outputs":[{"name":"_tournamentAddress","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"allTournaments","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_query","type":"bytes32"},{"name":"_sender","type":"address"}],"name":"Query","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"mySubmissions","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_tournamentAddress","type":"address"}],"name":"getTournament_IsMine","outputs":[{"name":"_isMine","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_toIgnore","type":"uint256"}],"name":"prepareBalance","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tournamentAddress","type":"address"}],"name":"enterTournament","outputs":[{"name":"_success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_matryxTournamentFactoryAddress","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_owner","type":"address"},{"indexed":false,"name":"_tournamentAddress","type":"address"},{"indexed":false,"name":"_tournamentName","type":"string"},{"indexed":false,"name":"_externalAddress","type":"bytes32"},{"indexed":false,"name":"_MTXReward","type":"uint256"},{"indexed":false,"name":"_entryFee","type":"uint256"}],"name":"TournamentCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_owner","type":"address"},{"indexed":false,"name":"_tournamentAddress","type":"address"},{"indexed":false,"name":"_tournamentName","type":"string"},{"indexed":false,"name":"_externalAddress","type":"bytes32"},{"indexed":false,"name":"_MTXReward","type":"uint256"},{"indexed":false,"name":"_entryFee","type":"uint256"}],"name":"TournamentOpened","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_tournamentAddress","type":"address"},{"indexed":false,"name":"_finalRoundNumber","type":"uint256"},{"indexed":false,"name":"_winningSubmissionIndex","type":"uint256"}],"name":"TournamentClosed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"queryID","type":"string"}],"name":"QueryID","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"sender","type":"address"}],"name":"QueryPerformed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"storedResponse","type":"uint256"}],"name":"StoredResponse","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"response","type":"uint256"}],"name":"ObtainedResponse","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newResponse","type":"uint256"},{"indexed":false,"name":"oldResponse","type":"uint256"}],"name":"FailedToStore","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"}],"name":"QueryID","type":"event"}];
@@ -85,6 +92,11 @@ platformCalls.getTouramentById = function(_tournament_id){
 
 
 //Activity Code
+
+//TODO use web3 to get all previous events for each of the situations (from block to this block) and maybe filter the event
+//TODO https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethfilter
+//TODO We want to assume that all the data we needs comes from the platform events being triggered and no additional searches are needed
+
 platformCalls.activity = function(){
     return new Promise((resolve, reject) => {
         // "news": "0xb794f5ea0ba39494ce839613fffba74279579268 created a new bounty: \"Solve Diabetes\""
@@ -182,13 +194,110 @@ platformCalls.activity = function(){
            console.log("error in jsoncreator.js: " + error);
         });
 
-
-
-
-
-
     }
 )};
+
+/*
+Get Activity using web3ethfilter
+
+Input params:
+String|Object - The string "latest" or "pending" to watch for changes in the latest block or pending transactions respectively. Or a filter options object as follows:
+fromBlock: Number|String - The number of the earliest block (latest may be given to mean the most recent and pending currently mining, block). By default latest.
+toBlock: Number|String - The number of the latest block (latest may be given to mean the most recent and pending currently mining, block). By default latest.
+address: String - An address or a list of addresses to only get logs from particular account(s).
+topics: Array of Strings - An array of values which must each appear in the log entries. The order is important, if you want to leave topics out use null, e.g. [null, '0x00...']. You can also pass another array for each topic with options for that topic e.g. [null, ['option1', 'option2']]
+
+
+*/
+
+
+platformCalls.getActivity2 = function(){
+  return new Promise((resolve, reject) => {
+   // contract.methods.tournamentByAddress(42 + _tournament_id).call({}, (err, res) => {
+
+   web3.eth.filter(options, function(error, result){
+  if (!error)
+    console.log(result);
+});
+     if (err) reject(err)
+     else
+       resolve({
+         _tournament_id: parseInt(res['0']),
+         title: res['1'],
+         description: res['2'],
+         bounty: parseFloat(res['3'])
+     });
+   });
+});
+}
+
+
+/*
+Logic for View all tournaments
+var MatryxPlatform = web3.eth.contract(MatryxPlatform.abi);
+var platform = MyContract.at(MatryxPlatform.address);
+
+var TournamentCreatedEvent = platform.events.TournamentCreated({fromBlock: 0, toBlock: 'latest'});
+
+var allTournaments = TournamentCreatedEvent.get(function(error, logs){
+
+	var allDetails = {};
+	for(var i = 0; i < logs.length; i++)
+	{
+		var log = logs[i];
+
+		var discipline = log.returnValues[0];
+		var owner = log.returnValues[1];
+		var tournamentAddress = log.returnValues[2];
+		var tournamentTitle = log.returnValues[3];
+		var externalAddress = log.returnValues[4];
+		var MTXReward = log.returnValues[5];
+		var entryFee = log.returnValues[6];
+
+		var tournamentDetails =
+		{
+			"tournamentTitle": tournamentTitle,
+			"mtx": MTXReward,
+			"tournamentDescription": "",
+			"category": discipline,
+			"totalRounds": 0,
+			"currentRound": 0,
+			"numberOfParticipants": 0,
+			"ipType": ""
+		};
+
+		ipfs.files.cat(ipfsPath, function (err, file) {
+			if (err) {
+			throw err
+			}
+
+			var fileAsString = file.toString('utf8');
+			var firstLine = fileAsString.split("\n")[0];
+			tournamentDetails["tournamentDescription"] = firstLine;
+
+			tournament.methods.maxRounds().then((receipt) => {
+		  		tournamentDetails["totalRounds"] = receipt;
+
+		  		tournament.methods.rounds().then((receipt)=> {
+					tournamentDetails["currentRound"] = receipt.length;
+
+					tournament.methods.entrantCount().then((receipt) => {
+						tournamentDetails["numberOfParticipants"] = receipt;
+
+						allDetails.push(tournamentDetails);
+						if(i == logs.length - 1)
+						{
+							return allTournaments;
+						}
+					});
+				});
+			})
+		})
+	}
+});
+
+
+*/
 
 
 // Get the latest Matryx Platform contract address and abi
