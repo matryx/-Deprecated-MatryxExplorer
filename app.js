336c18fc56258ab6dcdafb9055c51dea656cc682
Austin Gil <austin@stegosource.com>
2018-03-06 10:50:40 -0800

    Adding cors package

--- app.js
+++ app.js
@@ -1,64 +1,57 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv').config()
 
-
 //process.env.DB_HOST
-const externalApiCalls = require('./api/controllers/eth/externalApiCalls');
+const externalApiCalls = require('./api/controllers/eth/externalApiCalls')
 
 //Routes
-const platformRoutes = require('./api/routes/platform');
-const tournamentRoutes = require('./api/routes/tournaments');
-const roundRoutes = require('./api/routes/rounds');
+const platformRoutes = require('./api/routes/platform')
+const tournamentRoutes = require('./api/routes/tournaments')
+const roundRoutes = require('./api/routes/rounds')
 
+//tempAPI routes active
+const tempPlatformRoutes = require('./tempApi/platform')
+const tempTournamentRoutes = require('./tempApi/tournaments')
+const tempRoundRoutes = require('./tempApi/rounds')
+const tempActivityRoutes = require('./tempApi/activity')
 
-    //tempAPI routes active
-    const tempPlatformRoutes = require('./tempApi/platform');
-    const tempTournamentRoutes = require('./tempApi/tournaments');
-    const tempRoundRoutes = require('./tempApi/rounds');
-    const tempActivityRoutes = require('./tempApi/activity');
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-
-app.use('/platform', platformRoutes);
-app.use('/tournament', tournamentRoutes);
-app.use('/rounds', roundRoutes);
+app.use('/platform', platformRoutes)
+app.use('/tournament', tournamentRoutes)
+app.use('/rounds', roundRoutes)
 //TODO decide on controller split
 
-    //tempAPI routes active
-    app.use('/tempAPI/platform', tempPlatformRoutes);
-    app.use('/tempAPI/tournament', tempTournamentRoutes);
-    app.use('/tempAPI/rounds', tempRoundRoutes);
-    app.use('/tempAPI/activity', tempActivityRoutes);
+//tempAPI routes active
+app.use('/tempAPI/platform', tempPlatformRoutes)
+app.use('/tempAPI/tournament', tempTournamentRoutes)
+app.use('/tempAPI/rounds', tempRoundRoutes)
+app.use('/tempAPI/activity', tempActivityRoutes)
 //TODO add more temp APIs
-
 
 //TODO add more error handling and timeout setting
 
 //Error handling
-app.use((req, res, next) =>{
-    const error = new Error( 'Not Found');
-    error.status = 404
-    next(error)
-});
+app.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
 
-app.use((error, req, res, next) =>{
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
-
-
-
-module.exports = app;
+module.exports = app
