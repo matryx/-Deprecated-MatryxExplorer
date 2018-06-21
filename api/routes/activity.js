2c7016e5f26d3ea6a498065e619f33ad5d56f952
Alex McNerney <ajm13@me.com>
2018-06-21 14:38:21 -0700

    Refactor activity.js

--- api/routes/activity.js
+++ api/routes/activity.js
@@ -10,14 +10,14 @@ const router = express.Router()
 
 // const ethPlatform = require('../controllers/gateway/platformCalls')
 const matryxPlatformCalls = require('../controllers/gateway/matryxPlatformCalls')
+const { errorHelper } = require('../helpers/responseHelpers')
 
 // Finish Backend
 router.get('/', (req, res, next) => {
-  matryxPlatformCalls.getPlatformActivity().then(function (result) {
-    res.status(200).json({
-      activity: result
-    })
-  })
+  matryxPlatformCalls
+    .getPlatformActivity()
+    .then(activity => res.status(200).json({ activity }))
+    .catch(errorHelper(res))
 })
 
 module.exports = router
