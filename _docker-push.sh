f83e801395a240343040791f2bf96e6fa4137852
shessenauer <sam.hessenauer@gmail.com>
2018-06-22 13:13:40 -0700

    removed redundant docker push

--- _docker-push.sh
+++
@@ -1,5 +0,0 @@
-docker build -t matryx-alpha-explorer .
-
-docker tag matryx-alpha-explorer:latest 441665557124.dkr.ecr.us-west-1.amazonaws.com/matryx-alpha-explorer:latest
-
-docker push 441665557124.dkr.ecr.us-west-1.amazonaws.com/matryx-alpha-explorer:latest
