7c07871a1cec88c05af66ee4896f8381dff8cbcc
Alex McNerney <ajm13@me.com>
2018-08-14 13:06:38 -0700

    Changed Contract provider to ropsten

--- api/contracts/Contract.js
+++ api/contracts/Contract.js
@@ -1,7 +1,8 @@
 const ethers = require('ethers')
 
-const providerURL = process.env.CUSTOMRPC
-const provider = new ethers.providers.JsonRpcProvider(providerURL)
+// const providerURL = process.env.CUSTOMRPC
+// const provider = new ethers.providers.JsonRpcProvider(providerURL)
+const provider = new ethers.providers.InfuraProvider('ropsten', 'metamask')
 
 module.exports = class Contract {
   constructor(address, abi) {
